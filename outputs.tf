a8660eb65a798364fbbf8e00a679e9afbf1f65fb
Aaron Curtis <aaron.curtis@gatesfoundation.org>
2024-09-10 08:28:12 -0700

    fix: add description

--- outputs.tf
+++ outputs.tf
@@ -1,11 +1,14 @@
 output "name" {
-  value = azurerm_resource_group.rg.name
+  value       = azurerm_resource_group.rg.name
+  description = "The name of the resource group"
 }
 
 output "location" {
-  value = azurerm_resource_group.rg.location
+  value       = azurerm_resource_group.rg.location
+  description = "The location of the resource group"
 }
 
 output "id" {
-  value = azurerm_resource_group.rg.id
+  value       = azurerm_resource_group.rg.id
+  description = "The id of the resource group"
 }
